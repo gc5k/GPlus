1cf64c31e6e069968e890303565982d7bab3c925
Zhixiang Zhu <zzxiang21cn@hotmail.com>
2018-07-29 16:32:22 +0900

    Made cpplint.sh check all the source files.

--- cpplint.sh
+++ cpplint.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-python cpplint.py --root=.. $1
+#
+# Check whether the source files follow Google C++ Coding Style.
+
+if [[ $# -gt 0 ]]; then
+  files=$@
+else
+  files=$(find . -path ./third_party -prune -printf '' -o \
+    -name "*.h" -print -o -name "*.cpp" -print)
+fi
+
+for file in ${files}; do
+  python cpplint.py --root=.. ${file}
+done
