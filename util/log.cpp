d5b0feb8f06bbe80d1fa3edd8db5ffcea1ccd961
Zhixiang Zhu <zzxiang21cn@hotmail.com>
2018-08-05 13:44:39 +0900

    Added timestamp in logs.

--- util/log.cpp
+++ util/log.cpp
@@ -10,14 +10,17 @@
 
 #include <iostream>
 #include <fstream>
+#include "third_party/boost/date_time/posix_time/posix_time_types.hpp"
 #include "third_party/boost/core/null_deleter.hpp"
 #include "third_party/boost/smart_ptr/shared_ptr.hpp"
 #include "third_party/boost/smart_ptr/make_shared_object.hpp"
-#include "third_party/boost/log/expressions.hpp"
 #include "third_party/boost/log/sinks/sync_frontend.hpp"
 #include "third_party/boost/log/sinks/text_ostream_backend.hpp"
 #include "third_party/boost/log/attributes.hpp"
 #include "third_party/boost/log/utility/setup/common_attributes.hpp"
+#include "third_party/boost/log/support/date_time.hpp"
+#include "third_party/boost/log/expressions.hpp"
+#include "third_party/boost/log/expressions/formatters/date_time.hpp"
 
 namespace logging = boost::log;
 namespace src = boost::log::sources;
@@ -30,8 +33,14 @@ namespace gplus {
 BOOST_LOG_ATTRIBUTE_KEYWORD(diagnostic_attr, "Diagnostic", bool)
 
 void InitLogging() {
+  using boost::posix_time::ptime;
+  auto formatter = expr::format("[%1%] %2%")
+      % expr::format_date_time<ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S")
+      % expr::message;
+
   // Construct the sink
   typedef sinks::synchronous_sink<sinks::text_ostream_backend> text_sink;
+
   boost::shared_ptr<text_sink> sink = boost::make_shared<text_sink>();
 
   // Add a stream to write log to
@@ -44,6 +53,7 @@ void InitLogging() {
   sink->locked_backend()->add_stream(stream);
 
   sink->set_filter(!expr::has_attr(diagnostic_attr));
+  sink->set_formatter(formatter);
 
   // Register the sink in the logging core
   logging::core::get()->add_sink(sink);
@@ -55,6 +65,7 @@ void InitLogging() {
   sink->locked_backend()->add_stream(
       boost::make_shared<std::ofstream>("gplus_diagnostic.log"));
   sink->set_filter(expr::has_attr(diagnostic_attr) && diagnostic_attr);
+  sink->set_formatter(formatter);
   logging::core::get()->add_sink(sink);
 
   logging::add_common_attributes();
