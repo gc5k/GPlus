4418a85ef2aded1a77346453fd6f76936ad573a6
Zhixiang Zhu <zzxiang21cn@hotmail.com>
2018-08-19 01:02:16 +0900

    Opened bed file.

--- data/bed_file.cpp
+++ data/bed_file.cpp
@@ -8,11 +8,20 @@
 
 #include "data/bed_file.h"
 
+#include <fstream>
+
+#include "util/log.h"
+
 namespace gplus {
 
 std::shared_ptr<BedFile> BedFile::Read(const std::string &file_name,
                                        size_t snp_count,
                                        size_t sample_count) {
+  std::ifstream in_stream(file_name, std::ios::binary);
+  if (!in_stream) {
+    GPLUS_LOG << "Cannot open the bed file '" << file_name << "'.";
+    exit(EXIT_FAILURE);
+  }
   std::shared_ptr<BedFile> bed_file(new BedFile);
   return bed_file;
 }
