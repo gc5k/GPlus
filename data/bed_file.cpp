37c090bbb6721655aba1b2fb90f5ce031d4a57d4
Zhixiang Zhu <zzxiang21cn@hotmail.com>
2018-08-19 23:33:52 +0900

    Read the first 3 bytes of bed file.

--- data/bed_file.cpp
+++ data/bed_file.cpp
@@ -22,6 +22,22 @@ std::shared_ptr<BedFile> BedFile::Read(const std::string &file_name,
     GPLUS_LOG << "Cannot open the bed file '" << file_name << "'.";
     exit(EXIT_FAILURE);
   }
+  
+  char magic[3];
+  if (!in_stream.read(magic, sizeof(magic))) {
+    GPLUS_LOG
+    << "Failed to read the first " << sizeof(magic) << " bytes of the bed file '"
+    << file_name << "'.";
+    exit(EXIT_FAILURE);
+  }
+  
+  if (0x1 == magic[2]) {
+    // variant major
+  } else {
+    GPLUS_LOG << "Sample major bed file is not supported yet.";
+    exit(EXIT_FAILURE);
+  }
+  
   std::shared_ptr<BedFile> bed_file(new BedFile);
   return bed_file;
 }
