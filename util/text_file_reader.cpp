d23ddfd97099d833fdc99765ea45b20e0f3da675
Zhixiang Zhu <zzxiang21cn@hotmail.com>
2018-10-07 21:01:17 +0900

    Add SafeGetLine to handle "\r", "\n" and "\r\n"

--- util/text_file_reader.cpp
+++ util/text_file_reader.cpp
@@ -22,6 +22,40 @@ using std::string;
 
 namespace gplus {
 
+// This function handles all three line endings ("\r", "\n" and "\r\n").
+// Copied from https://stackoverflow.com/a/6089413/444860
+static std::istream& SafeGetLine(std::istream& is, std::string& t) {
+  t.clear();
+  
+  // The characters in the stream are read one-by-one using a std::streambuf.
+  // That is faster than reading them one-by-one using the std::istream.
+  // Code that uses streambuf this way must be guarded by a sentry object.
+  // The sentry object performs various tasks,
+  // such as thread synchronization and updating the stream state.
+  
+  std::istream::sentry se(is, true);
+  std::streambuf* sb = is.rdbuf();
+  
+  for(;;) {
+    int c = sb->sbumpc();
+    switch (c) {
+      case '\n':
+        return is;
+      case '\r':
+        if(sb->sgetc() == '\n')
+          sb->sbumpc();
+        return is;
+      case std::streambuf::traits_type::eof():
+        // Also handle the case when the last line has no line ending
+        if(t.empty())
+          is.setstate(std::ios::eofbit);
+        return is;
+      default:
+        t += (char)c;
+    }
+  }
+}
+
 TextFileReader::TextFileReader(const std::string& file_description,
                                const std::string& file_name)
     : file_desc_("the " + file_description + " file '" + file_name + "'"),
@@ -39,7 +73,7 @@ TextFileReader::TextFileReader(const std::string& file_description,
 }
 
 bool TextFileReader::ReadNonEmptyLine() {
-  while (std::getline(in_stream_, line_)) {
+  while (SafeGetLine(in_stream_, line_)) {
     ++line_no_;
     boost::trim(line_);
     if (!line_.empty()) {
